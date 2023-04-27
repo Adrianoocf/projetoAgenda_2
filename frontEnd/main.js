bad10fa5a6fb55f8be10d095495872a2cab99ef6
adrianoocf <adriano.ocf@hotmail.com>
2023-04-27 13:53:18 -0300

    ok

--- frontEnd/main.js
+++ frontEnd/main.js
@@ -1,13 +1,13 @@
 /*
-
-para instalar dependencies no package json 
-... npm i --save-dev  @babel/core @babel/preset-env babel-loader webpack webpack-cli core-js css-loader regenerator-runtime style-loader
-
-npm i --save-dev webpack-cli webpack nodemon babel-loader @babel/preset-env @babel/core @babel/cli core-js regenerator-runtime  style-loader
-
-
+  para instalar dependencies no package json 
+  ... npm i --save-dev  @babel/core @babel/preset-env babel-loader webpack webpack-cli core-js css-loader regenerator-runtime style-loader
+  npm i --save-dev webpack-cli webpack nodemon babel-loader @babel/preset-env @babel/core @babel/cli core-js regenerator-runtime  style-loader
 */
 
+/*
+  url e senha do mongo
+  mongodb+srv://Adrianoocf:<password>@cluster0.oii3ojp.mongodb.net/?retryWrites=true&w=majority
+*/
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -15,22 +15,15 @@ import './assets/css/style.css';
 
 /* exemplos
 import { nome as nome_cliente, sobrenome, idade, soma, Pessoa } from './modulo1';
-// mei rafael
-
 const nome = 'Adriano';
 console.log(nome);
-
 console.log(`ola : ${nome_cliente} ${sobrenome} , tua idade é : ${idade}`);
 console.log('10+5 : ' + soma(10,5));
-
 const p1 = new Pessoa('Adriano','Ferreira')
 console.log(p1)
 */
 
-
-
 /*
-
  {
   "name": "webpack_professor",
   "version": "1.0.0",
@@ -61,5 +54,4 @@ console.log(p1)
     "express": "^4.18.2"
   }
 }
-
 */
--- routes.js
+++ routes.js
@@ -3,9 +3,12 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
  
+// rota da home
 route.get('/', homeController.paginaInicial); 
 route.post('/', homeController.trataPost); 
-route.get('/teste/:idUsuarios?/:parametro?', homeController.parametro); 
+
+// route.get('/teste/:idUsuarios?/:parametro?', homeController.parametro); 
+// rota de contato
 route.get('/contato', contatoController.contato); 
  
 module.exports = route;
