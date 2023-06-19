e449309994c2a0f56b36890400cbc6d9d6d6fdf0
adrianoocf <adriano.ocf@hotmail.com>
2023-06-19 20:08:06 -0300

    commit em 19/7

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// git do curso https://github.com/luizomf/curso-js
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -8,15 +9,37 @@ mongoose.connect(process.env.CONNECTIONSTRING)
         app.emit('pronto')
     })
     .catch( () => console.log('Ocorreu um erro'));
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
 const routes = require('./routes');
 const path = require('path');
-const meuMiddleware = require('./src/middleware/middleware'); 
+const { middlewareGlobal } = require('./src/middleware/middleware'); 
 
 app.use(express.urlencoded({extended: true}));
+
 app.use(express.static(path.resolve(__dirname,'public')))
+
+const sessionOptions = session({
+    secret: 'kfçlsmfsmfl',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000*60*60*24*7,
+    httpOnly: true
+    }
+});
+
+app.use(sessionOptions);
+app.use(flash());
+
 app.set('views', path.resolve(__dirname, 'src' , 'views'));
 app.set('view engine', 'ejs');
-app.use(meuMiddleware);
+
+app.use(middlewareGlobal);
 app.use(routes);
  
 app.on('pronto', () => {
--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,4 +1,5 @@
-module.exports = (req, res, next) => {
+exports.middlewareGlobal = (req, res, next) => {
     console.log('Entrou no middleware.');
+    res.locals.umaVariavelLocal = 'Este e o vlor da variavel local.'
     next();
 };
